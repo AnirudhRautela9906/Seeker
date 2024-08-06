3c522b73a9099f74bef653d33a8a3b6bfc610e32
Ayush Kumar <ayush9kumar9@gmail.com>
2024-08-06 11:46:09 +0530

    Started with frontend, added Navbar, Footer and Home Page

---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../Home_images/logo.png";
+import "./Style.scss";
+const Navbar = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  return (
+    <>
+      <div className="nav1">
+        <div className="nav1-1">
+          <img src={logo} alt="" />
+          <Link to="/">Home</Link>
+          <Link to="/Contact">Contact</Link>
+        </div>
+        <div className="nav1-2">
+          <Link to="/Login">Log in</Link>
+          <Link to="/Signup">Sign Up</Link>
+        </div>
+        {/* <div className='nav1-p' onMouseEnter={()=>{setShowDropdown((prevState)=>{return true});
+        }} onMouseLeave={()=>{setShowDropdown((prevState)=>{return false})}} >
+            <Link style={{width:"100px"}} to="/Profile"   >Profile</Link>
+            {showDropdown && <div>
+            <div style={{border:"2px solid black", height:"100px", width:"100px",position:'absolute'}} ></div>    
+        </div>} */}
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
