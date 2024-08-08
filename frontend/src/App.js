eb071e8adad846d15d5bd6f3a9636eb9e13d091f
Anant-Singhal <anantsinghal1998@gmail.com>
2024-08-08 20:48:18 +0530

    added client home page and jobCard component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
 import Register from "./pages/signup/SignUpForm.jsx";
 import Login from "./pages/login/Login.jsx";
+import Profile from './pages/profile/Profile.jsx'
 function App() {
   
   return (
@@ -11,7 +12,8 @@ function App() {
       <Router>
         
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={< Home />} />
+          <Route path="/profile" element={< Profile />} />
           <Route path="/Signup" element={<Register />} />
           <Route path="/Login" element={<Login />} />
         </Routes>
