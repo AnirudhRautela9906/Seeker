3f21652a06f4a380293d23aea9beb3f71f7b6448
Ayush Kumar <ayush9kumar9@gmail.com>
2024-08-30 01:34:49 +0530

    Added env variables in Run Configurations

--- backend/src/main/java/com/seeker/CorsConfig.java
+++ backend/src/main/java/com/seeker/CorsConfig.java
@@ -1,18 +1,24 @@
 package com.seeker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+	
+	@Value("${frontend.url}")
+	private String frontendUrl;
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**") // Apply CORS to all endpoints
-				.allowedOrigins("http://localhost:3000","http://localhost:3001") // Replace with your client-side URL
+				.allowedOrigins(frontendUrl) 
 				.allowedMethods("GET", "POST", "PUT", "DELETE") // Specify allowed methods
 				.allowedHeaders("Content-Type", "Authorization") // Specify allowed headers
+				.allowedHeaders("Authorization")
+	            .exposedHeaders("Authorization")
 				.allowCredentials(true); // Allow credentials (cookies) to be included
 	}
 }
